8cbb5ed9c6ce08750596fdd35c0b899f9efddf1f
Derek Rath <derekrath@Dereks-MacBook-Pro.local>
2024-01-16 16:38:16 -0800

    added migration files

---
+++ migrations/migrations/20240117001348_create_users.js
@@ -0,0 +1,22 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.string('first_name', 255);
+    table.string('last_name', 255);
+    table.string('username', 255).unique().notNullable;
+    table.string('password_hash', 255).notNullable();
+    table.timestamps(true, true);
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
---
+++ migrations/migrations/20240117002332_create_items.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('items', table => {
+      table.increments('id');
+      table.integer('user_id').unsigned().references('id').inTable('users');
+      table.string('name', 255).notNullable();
+      table.text('description');
+      table.integer('quantity').unsigned().notNullable();
+      table.timestamps(true, true);
+    })
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+  
