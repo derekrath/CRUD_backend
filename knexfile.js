44586416425e886a1d0091a5fa56a8dd89c8d104
Derek Rath <derekrath@Dereks-MacBook-Pro.local>
2024-01-22 23:09:17 -0600

    fixed an issue in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,43 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 // const connectionString = process.env.DATABASE_URL; //hosting service should use DATABASE_URL environment variable. Double check before production.
-console.log('connectionString:', connectionString, 'env environment', process.env.NODE_ENV); //should show the production or dev environment variables
+console.log(
+  "connectionString:",
+  connectionString,
+  "env environment",
+  process.env.NODE_ENV
+); //should show the production or dev environment variables
 
 module.exports = {
-    development: {
-        client: 'pg',
-        connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
-        migrations: {
-            directory: './migrations',
-        },
-        seeds: {
-            directory: './seeds'
-        }
+  development: {
+    client: "pg",
+    connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
+    migrations: {
+      directory: "./migrations",
     },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
 
-    staging: {
-        client: 'pg',
-        connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
-        migrations: {
-            directory: './migrations',
-        },
-        seeds: {
-            directory: './seeds'
-        }
+  staging: {
+    client: "pg",
+    connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
+    migrations: {
+      directory: "./migrations",
     },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
 
-    production: {
-        client: 'pg',
-        connection: {
-            connectionString: process.env.DATABASE_URL,
-            ssl: { rejectUnauthorized: false }
-        },
-        // connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
     },
-            migrations: {
-            directory: './migrations',
-        },
-        seeds: {
-            directory: './seeds'
-        }
-    }
-}
+    // connection: connectionString, //might need to remove reject unauthorized ssl if connection doesnt work
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
+  },
+};
